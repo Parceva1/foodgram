2bea428e88c7e68e06ba167f967bc88fefb49305
Stas Kuzmin <stassskuz@gmail.com>
2025-01-20 01:10:37 +0300

    admin fiches v3

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from recipes.models import Tag
-
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug')
-    search_fields = ('name', 'slug')
-    prepopulated_fields = {'slug': ('name',)}
